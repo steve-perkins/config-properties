c408cbcfadfe992f481b684c3391b63042bc4934
Steve Perkins <steve@steveperkins.com>
2017-02-13 10:52:09 -0500

    Initial commit

---
+++ src/main/java/com/steveperkins/config/Processor.java
@@ -0,0 +1,17 @@
+package com.steveperkins.config;
+
+/**
+ * TODO: Document
+ */
+public class Processor {
+
+    /**
+     * TODO: Document
+     *
+     * @param args
+     */
+    public static void main(String args[]) {
+        System.out.println("Processor ran!");
+    }
+
+}
